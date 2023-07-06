3134e9b98425780b1e54f5e498c2c66e25abf56c
zzl <zhuangzhonglong@live.com>
2023-07-07 01:06:52 +0800

    Add screenshots

--- drawing/gdi/brush.go
+++
@@ -1,5 +0,0 @@
-package gdi
-
-type Brush struct {
-
-}
--- forms/input.go
+++
@@ -1,8 +0,0 @@
-package forms
-
-type Input interface {
-	GetValue() any
-	SetValue(any)
-	Focus()
-	GetOnValueChange() *SimpleEvent //todo:clearify only fire on user action?
-}
